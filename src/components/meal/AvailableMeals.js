266d718380774d74c5af21715d770d80200d8eb6
Mashawabupr <marikanik1999@gmail.com>
2022-10-19 17:05:03 +0300

    added fetch data

--- src/components/meal/AvailableMeals.js
+++ src/components/meal/AvailableMeals.js
@@ -1,44 +1,38 @@
 import styles from "./AvailableMeals.module.css";
 import Card from "../UI/Card.js";
+import { useState, useEffect } from "react";
 import MealItem from "./MealItem.js";
-let meals = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
-let mealsList = meals.map((el) => {
-  return (
-    <MealItem
-      key={el.id}
-      id={el.id}
-      name={el.name}
-      description={el.description}
-      price={el.price}
-    />
-  );
-});
 let AvailableMeals = () => {
+  let [fetchMeal, setFetch] = useState("");
+  useEffect(() => {
+    fetch("https://food-app-19d70-default-rtdb.firebaseio.com/food.json")
+      .then((response) => response.json())
+      .then((data) => setFetch(data))
+      .catch(() => console.log("error"));
+  }, []);
+  let newFetchMeal = [];
+  for (let key in fetchMeal) {
+    if (fetchMeal[key]) {
+      newFetchMeal.push({
+        id: fetchMeal[key].id,
+        description: fetchMeal[key].description,
+        name: fetchMeal[key].name,
+        price: fetchMeal[key].price,
+      });
+    }
+  }
+  let mealsList = newFetchMeal.map((el) => {
+    return (
+      <MealItem
+        key={el.id}
+        id={el.id}
+        name={el.name}
+        description={el.description}
+        price={el.price}
+      />
+    );
+  });
+
   return (
     <Card className={styles.meals}>
       <ul>{mealsList}</ul>
